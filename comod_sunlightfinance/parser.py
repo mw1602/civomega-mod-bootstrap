a4547e8469c7c9a1e989f5d9f44607450b667ede
Masha Westerlund <masha.westerlund@gmail.com>
2015-02-07 15:02:27 -0500

    added legislator query

--- comod_sunlightfinance/parser.py
+++ comod_sunlightfinance/parser.py
@@ -10,6 +10,46 @@ from .patterns import PATTERNS
 import json
 from django.template import loader, Context
 from random import Random
+import requests
+import os
+
+def get_api_key():
+    key = None
+    try:
+        key = settings.SUNLIGHT_API_KEY
+    except:
+        pass
+    if 'SUNLIGHT_API_KEY' in os.environ:
+        key = os.environ['SUNLIGHT_API_KEY']
+    if key == None:
+        raise Exception("To use this module, you must have a Sunlight API Key. See the readme here: https://github.com/CivOmega/civomega-mod-sunlightbills/")
+    else:
+        return key
+
+
+def legislator_search(**kwargs): #looks up a congressperson given the keywords
+
+    if kwargs != {}:
+        url = 'https://congress.api.sunlightfoundation.com/legislators/?apikey=%s' % (get_api_key())
+        params  = ''.join('&%s=%s' % pair for pair in kwargs.iteritems())
+        resp = requests.get(url+params)
+        return resp.json()
+    else:
+        return {}
+
+def fec_query_by_legislator(names):
+    for name in names:
+
+
+        
+        legislator = legislator_search(name=name)
+        legislator_fec_ids = legislator['results'][0]['fec_ids']
+
+
+
+
+
+
 
 
 ############################################################
